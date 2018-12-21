f2932162e350dc2a70d938b414360ae4df8837c1
renyifan <renyifan@iairfly.com>
2018-12-21 11:54:58 +0800

    [dev] Review shell(four).

---
+++ shell/4_array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+arr=(1 2 3);
+
+echo "arr[0] = ${arr[0]}"
+echo "arr[1] = ${arr[1]}"
+echo "arr[2] = ${arr[2]}"
+
+echo "arr length = ${#arr[*]}"
+echo "arr all value = ${arr[*]}"
+echo "arr all value = ${arr[@]}"
